2356948dae8e7beb3902559db27b4207cdabb02c
Tim John <chezzyt13@gmail.com>
2020-02-26 19:37:47 +0000

    unignore

---
+++ stirlandscricket/build/precache-manifest.e209e547c7f402e3f30acb317e68630d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f559a94132c1da33cd81f7798e5868a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "024f715f253d5ca48313",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eda64f7e1edae3de82d6",
+    "url": "/static/js/2.744cb14c.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/static/js/2.744cb14c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024f715f253d5ca48313",
+    "url": "/static/js/main.acb751e6.chunk.js"
+  },
+  {
+    "revision": "b8ff46f0e5ccb372bb5a",
+    "url": "/static/js/runtime-main.34a3d2c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
