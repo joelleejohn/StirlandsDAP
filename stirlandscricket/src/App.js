7f38462dba96d105bc834bfa3584410bdb47e0d4
Tim John <Tim.John@LighthouseSystems.com>
2020-03-10 11:04:44 +0000

    Debugging fetch at work

--- stirlandscricket/src/App.js
+++ stirlandscricket/src/App.js
@@ -6,37 +6,35 @@ import './App.css';
 class App extends Component {
   constructor(){
     super();
-    this.state = { data: undefined };
+    this.state = { userroles: undefined };
   }
 
   async loadPlayers(){
     let formData = new FormData();
     formData.append('queryMethod', 'getAllPlayers');
-    const resp = await fetch('http://localhost/StirlandsDAP/stirlandscricket/src/api/dbInferface.php', {  method: 'POST', body: formData}).then(response => {this.state.data = response; return response.json()}).then(data => console.log(data));
+    await fetch('http://localhost:8012/StirlandsDAP/stirlandscricket/src/api/dbInferface.php', {  method: 'POST', body: formData})
+                  .then(async (response) => {this.setState({userroles: (await response.json()).data})});
   }
 
   async componentDidMount(){
-    await this.loadPlayers();
+    console.log(this.state.userroles);
+    (await this.loadPlayers());
+    console.log('After await:');
+    console.log(this.state.userroles);
   }
-  
+
   render() {
+    let items = [];
+    console.log('in render:');
+    console.log(this.state.userroles);
+    if (this.state.userroles)
+      this.state.userroles.forEach(item => items.push(<p>{item.rfuserrole}</p>));
     return (
       <div className="App">
         <Menu />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {items}
         </header>
       </div>
     );
