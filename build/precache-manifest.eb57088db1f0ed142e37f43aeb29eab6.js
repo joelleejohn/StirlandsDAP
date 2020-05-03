9fbd779d187a3460dcc44f866a2fe9872f7cf1e9
Tim John <chezzyt13@gmail.com>
2020-05-03 22:06:39 +0100

    Live attempt no 2

---
+++ build/precache-manifest.eb57088db1f0ed142e37f43aeb29eab6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be1e11bf5eaa0fbde34fc149b84673e",
+    "url": "/DAP522/build/index.html"
+  },
+  {
+    "revision": "4cff50e55b2a30fe8234",
+    "url": "/DAP522/build/static/css/main.8b8af5af.chunk.css"
+  },
+  {
+    "revision": "99d900105330e528b7c2",
+    "url": "/DAP522/build/static/js/2.6b66ead1.chunk.js"
+  },
+  {
+    "revision": "1fedcf5c84e2f9b04a7e15ecdecddbbe",
+    "url": "/DAP522/build/static/js/2.6b66ead1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cff50e55b2a30fe8234",
+    "url": "/DAP522/build/static/js/main.888253fe.chunk.js"
+  },
+  {
+    "revision": "fa3f7a8b382ee756e48b",
+    "url": "/DAP522/build/static/js/runtime-main.e400a4bf.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/DAP522/build/static/media/logo.ee7cd8ed.svg"
+  }
+]);
